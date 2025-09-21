3245b1bff1e7e0ca4e850553faf83f656ed323ae
BlessE1 <blessingoe16@gmail.com>
2025-09-21 19:21:20 +0100

    check docs 2

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from copy_source_to_dest import copy_from_source_to_destination
 from generate_pages_recursive import generate_pages_recursive
 
 def main():
-   basepath = sys.argv[1] if len(sys.argv) <= 1 else "/"
+   basepath = sys.argv[1] if len(sys.argv) == 2 else "/"
    copy_from_source_to_destination(".", "static", "docs")
    generate_pages_recursive("content", "template.html", "docs", basepath)
 
